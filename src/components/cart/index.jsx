a12deb9f9cbfd6725882fc86589115fe34ef51d9
Huynh <conghuynhho12@gmail.com>
2021-03-04 19:52:23 +0700

    update to deploy

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CartProduct from './cart-product/'
 import './styles.scss'
 
 function Cart(props) {
-  const [isOpen, setIsOpen] = useState(false);
-  
+  // const [isOpen] = useState(false);
+
   return (
     <div className="cart">
       {/* {isOpen && (
@@ -13,14 +13,12 @@ function Cart(props) {
         </div>
       )} */}
 
-      {!isOpen && (
-        <div className="bag cart__open-btn">
-          <span className="cart-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" id="i-cart" viewBox="0 0 28 28" fill="none"><path d="M22.167 8.167h-3.5V7a4.667 4.667 0 00-9.334 0v1.167h-3.5a1.167 1.167 0 00-1.166 1.166v12.834a3.5 3.5 0 003.5 3.5h11.666a3.5 3.5 0 003.5-3.5V9.333a1.167 1.167 0 00-1.166-1.166zM11.667 7a2.333 2.333 0 114.666 0v1.167h-4.666V7zM21 22.167a1.167 1.167 0 01-1.167 1.166H8.167A1.167 1.167 0 017 22.167V10.5h2.333v1.167a1.167 1.167 0 002.334 0V10.5h4.666v1.167a1.167 1.167 0 102.334 0V10.5H21v11.667z" fill="currentColor"></path></svg>
-          </span>
-          <span className="bag__quantity">{2}</span>
-        </div>
-      )}
+      <div className="bag cart__open-btn">
+        <span className="cart-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" id="i-cart" viewBox="0 0 28 28" fill="none"><path d="M22.167 8.167h-3.5V7a4.667 4.667 0 00-9.334 0v1.167h-3.5a1.167 1.167 0 00-1.166 1.166v12.834a3.5 3.5 0 003.5 3.5h11.666a3.5 3.5 0 003.5-3.5V9.333a1.167 1.167 0 00-1.166-1.166zM11.667 7a2.333 2.333 0 114.666 0v1.167h-4.666V7zM21 22.167a1.167 1.167 0 01-1.167 1.166H8.167A1.167 1.167 0 017 22.167V10.5h2.333v1.167a1.167 1.167 0 002.334 0V10.5h4.666v1.167a1.167 1.167 0 102.334 0V10.5H21v11.667z" fill="currentColor"></path></svg>
+        </span>
+        <span className="bag__quantity">{2}</span>
+      </div>
 
       <div className="cart-content-wrapper">
         <div className="cart-header">
@@ -31,17 +29,17 @@ function Cart(props) {
         </div>
         <div className="cart-body">
           <div className="cart-item-list">
-            <CartProduct/>
-            <CartProduct/>
-            <CartProduct/>
-            <CartProduct/>
-            <CartProduct/>
-            <CartProduct/>
+            <CartProduct />
+            <CartProduct />
+            <CartProduct />
+            <CartProduct />
+            <CartProduct />
+            <CartProduct />
           </div>
         </div>
         <div className="cart-footer">
           <div className="check-out-btn">
-            Thanh toán: 
+            Thanh toán:
             <span> 29.000.000đ</span>
           </div>
         </div>
